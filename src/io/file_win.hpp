9387317bbcada6e8eb3d516e70f4f1abfef15894
bggd <gameovera@live.com>
2019-10-19 22:45:10 +0900

    file

--- src/io/file_win.hpp
+++ src/io/file_win.hpp
@@ -11,41 +11,40 @@ struct FileWin {
 
   HANDLE handle = 0;
   DWORD num_bytes = 0;
+};
 
-  void open(const char* path) {
-    assert(this->handle == 0);
-    this->handle = CreateFileA(path, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    assert(this->handle != 0);
-    assert(this->handle != INVALID_HANDLE_VALUE);
-    this->num_bytes = this->get_file_size();
-  }
+void file_win_open(FileWin* f, const char* path) {
+  assert(f->handle == 0);
+  f->handle = CreateFileA(path, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+  assert(f->handle != 0);
+  assert(f->handle != INVALID_HANDLE_VALUE);
+  f->num_bytes = GetFileSize(f->handle, NULL);
+  assert(f->num_bytes != 0);
+  assert(f->num_bytes != INVALID_FILE_SIZE);
+}
 
-  void close() {
-    assert(this->handle != 0 && this->handle != INVALID_HANDLE_VALUE);
-    BOOL b = CloseHandle(this->handle);
-    assert(b);
-    this->handle = 0;
-  }
+void file_win_close(FileWin* f) {
+  assert(f->handle != 0 && f->handle != INVALID_HANDLE_VALUE);
+  BOOL b = CloseHandle(f->handle);
+  assert(b);
+  f->handle = 0;
+  f->num_bytes = 0;
+}
 
-  uint32_t get_file_size() {
-    assert(this->handle != 0 && this->handle != INVALID_HANDLE_VALUE);
-    DWORD r = GetFileSize(this->handle, NULL);
-    assert(r != 0);
-    assert(r != INVALID_FILE_SIZE);
-    return r;
-  }
+uint32_t file_win_get_file_size(FileWin* f) {
+  assert(f->handle != 0 && f->handle != INVALID_HANDLE_VALUE);
+  return f->num_bytes;
+}
 
-  void* read() {
-    DWORD readed_bytes;
-    void* buffer = malloc(sizeof(char) * this->num_bytes + 1);
-    assert(buffer);
-    BOOL b = ReadFile(this->handle, buffer, this->num_bytes, &readed_bytes, NULL);
-    assert(b);
-    assert(this->num_bytes == readed_bytes);
-    ((char*)buffer)[this->num_bytes] = '\0';
-    return buffer;
-  }
-}; // struct FileWin
+void file_win_read(FileWin* f, void* dst) {
+  assert(f->handle != 0 && f->handle != INVALID_HANDLE_VALUE);
+  assert(f->num_bytes > 0);
+  assert(dst);
+  DWORD readed_bytes;
+  BOOL b = ReadFile(f->handle, dst, f->num_bytes, &readed_bytes, NULL);
+  assert(b);
+  assert(f->num_bytes == readed_bytes);
+}
 
 } // namespace moge
 
