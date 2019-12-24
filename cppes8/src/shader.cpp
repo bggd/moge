b6c1f6f52599f0bab13966d07e693d3c4a616c00
bggd <gameovera@live.com>
2019-12-24 15:46:29 +0900

    Fix invalid arg

--- cppes8/src/shader.cpp
+++ cppes8/src/shader.cpp
@@ -7,7 +7,7 @@ cppes8::shader::ShaderHandle cppes8::shader::create(cppes8::CPPES8& gamelib, cpp
   assert(shader_decl.vertex_shader);
   assert(shader_decl.pixel_shader);
 
-  moge::InputLayoutD3D11 input_layout = gamelib.render.create_input_layout((const moge::RenderMinimalInputLayoutD3D11*)shader_decl.array_of_input_layout, 1, shader_decl.vertex_shader, shader_decl.vertex_shader_len);
+  moge::InputLayoutD3D11 input_layout = gamelib.render.create_input_layout((const moge::RenderMinimalInputLayoutD3D11*)shader_decl.array_of_input_layout, shader_decl.num_array_of_input_layout, shader_decl.vertex_shader, shader_decl.vertex_shader_len);
   moge::ShaderObjectD3D11 vertex_shader = gamelib.render.create_vertex_shader(shader_decl.vertex_shader, shader_decl.vertex_shader_len);
   moge::ShaderObjectD3D11 pixel_shader = gamelib.render.create_pixel_shader(shader_decl.pixel_shader, shader_decl.pixel_shader_len);
 
